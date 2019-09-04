38670fdc304592103d66765a7d2c8d2f69c6e48f
Caroline S-A <caroline.seilern@hotmail.com>
2019-09-04 19:26:18 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,6 +3,6 @@ def meal_choice(veg1, veg2, protein = "meat")
   phrase =  "A plate of #{protein} with #{veg1} and #{veg2}."
   puts "What a nutritious meal!"
   puts  phrase
-  return phrase
+  phrase
 end
 
